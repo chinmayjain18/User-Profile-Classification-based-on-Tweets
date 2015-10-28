914efea5f45f3d7a8d833e2612c68f57b67069ca
Julien Eid <jeid64@gmail.com>
2015-10-28 17:12:30 -0400

    Added initial input (unpickling files). Now have a list of User objects containing Tweet objects.

--- ps2_4_classify_gender_names.py
+++ ps2_4_classify_gender_names.py
@@ -3,6 +3,66 @@ from nltk.corpus import names
 import random
 from string import ascii_lowercase
 
+import dataStructures
+
+import pickle
+import sys, os
+
+# Input:
+# files = list of strings of filenames in a directory
+# filename = string of file we are looking for.
+# Returns:
+# None if file is not found, otherwise, returns filename of file that was looked for.
+def check_for_file(files, filename):
+    if filename in files:
+        index = files.index(filename)
+        return files[index]
+    else:
+        return None
+
+# Input:
+# root = root directory that file is in.
+# filename = file we are going to open
+# Returns:
+# None if filename is none, otherwise, returns unpickled_data from file.
+def unpickle_from_filename(root, filename):
+    if filename is None:
+        return None
+    else:
+        unpickled_data = pickle.load(open(os.path.join(root, filename), "rb"))
+        return unpickled_data
+
+# Input:
+# data_folder = string, foldername we are going to recursively traverse.
+# Returns:
+# List of User objects. There should be a User object per subfolder.
+def load_data(data_folder):
+    user_list = []
+    for root, sub_folders, files in os.walk(data_folder):
+        ngramsFile = check_for_file(files, 'ngrams.pickl')
+        replacementsFile = check_for_file(files, 'replacements.pickl')
+        transformsFile = check_for_file(files, 'transforms.pickl')
+        tweetsFile = check_for_file(files, 'tweets.pickl')
+        userFile = check_for_file(files, 'user.pickl')
+
+        ngrams = unpickle_from_filename(root, ngramsFile)
+        replacements = unpickle_from_filename(root, replacementsFile)
+        transforms = unpickle_from_filename(root, transformsFile)
+        tweets = unpickle_from_filename(root, tweetsFile)
+        userInfo = unpickle_from_filename(root, userFile)
+
+        # Take tweets dictionary and turn to Tweet objects.
+        tweets_list = []
+        if tweets is not None:
+            for tweetId, value in tweets.items():
+                tweet = dataStructures.Tweet(id=tweetId, tokens=value["tokenized"], timestamp=value["time"], rawText=value["text"], numTokens=value["tokens"], numPunctuation=value["punc"])
+                tweets_list.append(tweet)
+
+        user = dataStructures.User(tweets=tweets_list, ngrams=ngrams, replacements=replacements, transforms=transforms, userInfo=userInfo)
+        user_list.append(user)
+    return user_list
+
+
 def gender_features(word):
     features = {'first two letters': word[:2], 'last letter': word[-1], 'first letter': word[0], 'length': len(word), 'last two': word[-2:], 'last three': word[-3:]}
     for letter in ascii_lowercase:
@@ -10,6 +70,9 @@ def gender_features(word):
     return features
 
 def main():
+    data_folder = sys.argv[1]
+    user_list = load_data(data_folder)
+
     labeled_names = ([(name, 'male') for name in names.words('male.txt')] + [(name, 'female') for name in names.words('female.txt')])
     random.shuffle(labeled_names)
     featuresets = [(gender_features(n), gender) for (n, gender) in labeled_names]
