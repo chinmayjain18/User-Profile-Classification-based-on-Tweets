a7f5c5672b215074de7d70c9ddd20b130770ddb8
Julien Eid <jeid64@gmail.com>
2015-10-30 12:14:40 -0400

    Removed old problem set 2 code.

--- ps2_4_classify_gender_names.py
+++ ps2_4_classify_gender_names.py
@@ -65,41 +65,9 @@ def load_data(data_folder):
         user_list.append(user)
     return user_list
 
-
-def gender_features(word):
-    features = {'first two letters': word[:2], 'last letter': word[-1], 'first letter': word[0], 'length': len(word), 'last two': word[-2:], 'last three': word[-3:]}
-    for letter in ascii_lowercase:
-        features["count(%s)" % letter] = word.lower().count(letter)
-    return features
-
 def main():
     data_folder = sys.argv[1]
     user_list = load_data(data_folder)
-
-    labeled_names = ([(name, 'male') for name in names.words('male.txt')] + [(name, 'female') for name in names.words('female.txt')])
-    random.shuffle(labeled_names)
-    featuresets = [(gender_features(n), gender) for (n, gender) in labeled_names]
-    train_set, test_set, final_set = featuresets[2382:], featuresets[1191:2382], featuresets[:1191]
-    classifier = nltk.NaiveBayesClassifier.train(train_set)
-
-    print("Accuracy for devtest_set:")
-    print(nltk.classify.accuracy(classifier, test_set))
-    print("")
-    print(classifier.show_most_informative_features(5))
-    print("")
-
-    error = []
-    count = 0
-    for word, tag in final_set:
-        count += 1
-        guess = classifier.classify(word)
-        if guess != tag:
-            #print "Wrong! " + label + " => " + guess
-            error.append(word)
-        #print error
-    print ("error rate in final set:", float(len(error)) / float(count))
-    print ("Accuracy for final set:")
-    print(nltk.classify.accuracy(classifier, final_set))
 
 if __name__ == '__main__':
     main()
